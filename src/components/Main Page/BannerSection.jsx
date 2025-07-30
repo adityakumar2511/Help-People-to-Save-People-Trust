5c49ce32215257f65c15c0664818964f764e7505
Aditya Kumar <adityak4724@gmail.com>
2025-07-31 00:53:52 +0530

    Fix: Missing HomeBanner image and folder name issue

--- src/components/Main Page/BannerSection.jsx
+++ src/components/Main Page/BannerSection.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import HomeBanner from "../../assets/HomeBanner.png";
+import bannerImg from "../../assets/homeBanner.png";
 
 const BannerSection = () => (
   <section className="relative w-full min-h-[50vh] lg:min-h-[80vh]">
     <div className="absolute inset-0 w-full h-full z-0">
       <img
-        src={HomeBanner}
+        src={bannerImg}
         alt="Banner Background"
         className="w-full h-full object-cover object-center"
       />
