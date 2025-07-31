c36d354bc2b184d06e14deeee6a05153559bc441
Aditya Kumar <adityak4724@gmail.com>
2025-07-31 22:48:47 +0530

    add

--- src/components/Main Page/CounterSection.jsx
+++ src/components/Main Page/CounterSection.jsx
@@ -42,7 +42,7 @@ const CounterSection = () => {
   return (
     <section
   ref={sectionRef}
-  className="w-full px-4 sm:px-6 py-16 sm:py-4 mt-10 bg-gradient-to-tr from-[#1e293b] to-[#0f172a] text-white"
+  className="w-full px-4 sm:px-6 py-16 mt-10 sm:py-4 bg-gradient-to-tr from-[#1e293b] to-[#0f172a] text-white"
 >
   <div className="max-w-7xl mx-auto p-4 sm:p-6 md:p-10">
     {/* Heading */}
