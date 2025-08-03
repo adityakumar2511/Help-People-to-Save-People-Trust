57e81a0e1a0ed4237475f7dc57b8ef7800c563a7
Aditya Kumar <adityak4724@gmail.com>
2025-08-03 14:50:58 +0530

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
   <section className="relative bg-gray-100 text-gray-800 dark:text-white min-h-screen mb-10">
   {/* Banner */}
-  <div className="w-full  bg-gradient-to-r from-[#ec7e35] to-[#0070C0] flex items-center justify-center px-4">
+  <div className="w-full h-48 bg-gradient-to-r from-[#ec7e35] to-[#0070C0] flex items-center justify-center px-4">
     <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-black text-center">
       About Us
     </h1>
